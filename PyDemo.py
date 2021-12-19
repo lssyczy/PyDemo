e32e242159c83b3eb24281718c33e9d9d56f7b62
shangtianting <490397050@qq.com>
2021-12-19 22:02:13 +0800

    init2

--- PyDemo.py
+++ PyDemo.py
@@ -1,4 +1,11 @@
 MyDic = {'Zhengyang': 100, 'Tianting':90, 'YouBao':80}
+MyList = ['China','Poland','Finland','France']
 
-print(MyDic['Zhengyang'])
+for key,value in MyDic.items():
+    print(key,value)
+
+for country in enumerate(MyList):
+    print(country)
+
+print(len(MyList))
 
